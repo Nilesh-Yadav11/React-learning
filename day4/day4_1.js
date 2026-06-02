631d9d241d7733e5811ea5e9feb9044c580954c6
Nilesh Yadav <ny172005@gmail.com>
2026-06-02 12:44:54 +0530

    Adding day4 work

---
+++ day4/day4_1.js
@@ -0,0 +1,31 @@
+// Vite --> parcel ki hi treh ek doosra bundler
+
+// go on vite and then copy npm create vite@latest
+
+// isko krte hi dekhoge ki vite ne ye sb khud hi bnakr de diya hoga html json sb jsx vgera jsx hi js ki file hogi 
+//ab main file kaun si hai jo ki index.html se connected hogi vo pata vhalega html se hi source src se dekh lena
+
+// THERE ARE TWO TYPES OF DEPENDENCIES in parcel 
+
+// i) normal one which is used in production
+// ii) dev dependecy which is used when writing for code
+
+//then npm install to download all dependencies or the node_modules
+
+// PS C:\Users\kadam\OneDrive\Desktop\nilesh_vscode\react\day4\vite-project1> npm install
+// aise krna 
+
+
+// run krne ka command 
+
+// npm run dev 
+
+//Ye saare commands humne liye hai vite pr jaakr google mein se 
+
+//Jo bhi info chahiye hogi vo jaakr lelena package.json se
+
+//Ab production rady code ko daalne ke liye phle dist folder banani pdegi jo ki hum banayenge from
+
+//npm run build se  --> AUR FIE EK DIST FOLDER BNEGA
+
+
